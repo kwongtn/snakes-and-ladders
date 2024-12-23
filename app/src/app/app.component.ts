b01ddc5db87f0cf131726ae61487dff1b63427f1
Kwong Tung Nan <tungnan5636@gmail.com>
2024-12-23 21:20:06 +0800

    Add ng zorro
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less',
+  styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'app';
+  isCollapsed = false;
 }
