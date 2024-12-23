336da32970099519179b108252dc4cb7e0de8224
Kwong Tung Nan <tungnan5636@gmail.com>
2024-12-23 21:16:48 +0800

    Update to angular 19
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,11 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import express from "express";
+import { dirname, join, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { APP_BASE_HREF } from "@angular/common";
+import { CommonEngine } from "@angular/ssr/node";
+
+import bootstrap from "./src/main.server";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -20,10 +22,13 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
